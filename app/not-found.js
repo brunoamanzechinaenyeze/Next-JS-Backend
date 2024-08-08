826d04f05c68b668077c1f71eb5db4278be93b6e
Amanze Bruno <heroicemblem69@gmail.com>
2024-08-08 10:14:47 +0100

    created a not-found page in next.js

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <h1 className='text-center font-mono justify-center mt-60'>404 - Page Not Found</h1>
+    </>
+  );
+};
+
+export default NotFound;
--- app/page.js
+++ app/page.js
@@ -1,23 +1,11 @@
 "use client";
-import { useRouter } from "next/navigation";
+
 
 const page = () => {
-  const router = useRouter();
-
-  const navigate = (page) => {
-    router.push(page);
-  };
 
   return (
-    <section>
-      <h1>useRouter</h1>
-      <button className="border px-2 py-4" onClick={() => navigate("about")}>
-        Go to About Page
-      </button> 
-      <button className="border px-2 py-4" onClick={() => navigate("login")}>
-        Go TO login Page
-      </button>
-    </section>
+    <>
+    </>
   );
 };
 
