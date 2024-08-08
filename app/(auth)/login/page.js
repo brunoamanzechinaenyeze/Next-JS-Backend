5f831f01e378acc0811ff366096a0807475f8bc5
Amanze Bruno <heroicemblem69@gmail.com>
2024-08-08 08:29:10 +0100

    creating app routers

---
+++ app/(auth)/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+    You've Logged into our page, Congratulations!!!
+    </div>
+  )
+}
+
+export default Login
---
+++ app/(auth)/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Space = () => {
+  return (
+    <div>
+      Authentication Page
+    </div>
+  )
+}
+
+export default Space
